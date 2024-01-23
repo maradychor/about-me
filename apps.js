0b98e086afaacf8c7144c4c139f69eaa304c8ae8
maradychor <mardchor@gmail.com>
2024-01-23 14:17:06 -0800

    done

--- apps.js
+++ apps.js
@@ -1,1 +1,15 @@
 console.log("JS is loaded!!!");
+let question1 = prompt('Do you want more children?');
+let answer1 = 'yes';
+
+let question2 = prompt('Do you want to get married?');
+let answer2 = 'yes';
+
+let question3 = prompt('Do you have a 5 year plan?'); 
+let answer3 = 'yes';
+
+let question4 = prompt('Do you want a new car?'); 
+let answer4 = 'yes'; 
+
+let question5 = prompt('Do you want to move states?');
+let answer5 = 'yes'; 
