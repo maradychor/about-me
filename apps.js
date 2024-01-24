1df1d385ad9e5d14b38276f2e3627a06712e3686
maradychor <mardchor@gmail.com>
2024-01-23 16:18:48 -0800

    finalize

--- apps.js
+++ apps.js
@@ -1,15 +1,56 @@
-console.log("JS is loaded!!!");
-let question1 = prompt('Do you want more children?');
+
+let question1 = prompt('Do you want more children?'); 
 let answer1 = 'yes';
+
+if (question1 === 'yes') {
+    console.log('yes'); 
+} else {
+    alert('sorry to hear');
+    console.log('wrong'); 
+}
+
 
 let question2 = prompt('Do you want to get married?');
 let answer2 = 'yes';
 
+if (question2 === 'yes') {
+    console.log('yes'); 
+} else {
+    alert('sorry to hear!!');
+    console.log('wrong'); 
+}
+
+
 let question3 = prompt('Do you have a 5 year plan?'); 
 let answer3 = 'yes';
+
+if (question3 === 'yes') {
+    console.log('yes'); 
+} else {
+    alert('start working on it!');
+    console.log('wrong'); 
+}
 
 let question4 = prompt('Do you want a new car?'); 
 let answer4 = 'yes'; 
 
+if (question4 === 'yes') {
+    console.log('yes'); 
+} else {
+    alert('AWWW, its OK!');
+    console.log('wrong'); 
+}
+
 let question5 = prompt('Do you want to move states?');
 let answer5 = 'yes'; 
+
+if (question5 === 'yes') {
+    console.log('yes'); 
+} else {
+    alert('stay where you are then');
+    console.log('wrong'); 
+}
+
+
+let question6 = prompt('What is your name?');
+document.write('<p>Hello! '+ question6 + '</p>');
